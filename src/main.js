52ad6cb2f6cf26b5861fb301994a52a7f3f98a61
Ivan Kashin <barbos1701@gmail.com>
2020-08-12 20:31:47 +0300

    Добавляет моки фильтра и отображение данных фильтра

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import {createUserScoreTemplate} from './view/user-score.js';
 import {createNavigationTemplate} from './view/navigation.js';
+import {createFilterTemplate} from './view/filter.js';
 import {createSortingTemplate} from './view/sorting.js';
 import {createFilmsPanelTemplate} from './view/films-panel.js';
 import {createFilmsListTemplate} from './view/films-list.js';
@@ -9,6 +10,7 @@ import {createFilmCardTemplate} from './view/film-card.js';
 import {createFilmDetailsPopupTemplate} from './view/film-details-popup.js';
 import {createStatisticsTemplate} from './view/statistics.js';
 import {generateFilmCard} from './mock/film-card.js';
+import {generateFilters} from './mock/filter.js';
 
 const CARDS_COUNT = 20;
 const EXTRA_CARDS_COUNT = 2;
@@ -17,9 +19,12 @@ const filmCards = new Array(CARDS_COUNT).fill().map(generateFilmCard);
 const extraFilmCardsTopRated = new Array(EXTRA_CARDS_COUNT).fill().map(generateFilmCard);
 const extraFilmCardsMostCommented = new Array(EXTRA_CARDS_COUNT).fill().map(generateFilmCard);
 
+const filters = generateFilters(filmCards);
+
 console.log(filmCards);
 console.log(extraFilmCardsTopRated);
 console.log(extraFilmCardsMostCommented);
+console.log(filters);
 
 
 const render = (container, template, place) => {
@@ -31,6 +36,10 @@ render(headerElement, createUserScoreTemplate(), `beforeend`);
 
 const mainElement = document.querySelector(`.main`);
 render(mainElement, createNavigationTemplate(), `beforeend`);
+
+const mainNavigation = mainElement.querySelector(`.main-navigation`);
+render(mainNavigation, createFilterTemplate(filters), `afterbegin`);
+
 render(mainElement, createSortingTemplate(), `beforeend`);
 render(mainElement, createFilmsPanelTemplate(), `beforeend`);
 
@@ -64,4 +73,4 @@ const footerElement = document.querySelector(`.footer`);
 const footerStatisticsElement = footerElement.querySelector(`.footer__statistics`);
 render(footerStatisticsElement, createStatisticsTemplate(), `beforeend`);
 
-render(footerElement, createFilmDetailsPopupTemplate(filmCards[0]), `afterend`);
+// render(footerElement, createFilmDetailsPopupTemplate(filmCards[0]), `afterend`);
