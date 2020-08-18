eddb00d1c9e16543aee9494f0b334d1cb118ca9c
Ivan Kashin <barbos1701@gmail.com>
2020-08-18 08:21:51 +0300

    Начинает переделку на классы, ЮзерСкор превращает в класс

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import {createUserScoreTemplate} from './view/user-score.js';
+import UserScoreView from './view/user-score.js';
 import {createNavigationTemplate} from './view/navigation.js';
 import {createFilterTemplate} from './view/filter.js';
 import {createSortingTemplate} from './view/sorting.js';
@@ -11,6 +11,7 @@ import {createFilmDetailsPopupTemplate} from './view/film-details-popup.js';
 import {createStatisticsTemplate} from './view/statistics.js';
 import {generateFilmCard} from './mock/film-card.js';
 import {generateFilters} from './mock/filter.js';
+import {render, renderTemplate, RenderPosition} from './utils.js';
 
 const CARDS_COUNT = 20;
 const EXTRA_CARDS_COUNT = 2;
@@ -22,35 +23,31 @@ const extraFilmCardsMostCommented = new Array(EXTRA_CARDS_COUNT).fill().map(gene
 
 const filters = generateFilters(filmCards);
 
-const render = (container, template, place) => {
-  container.insertAdjacentHTML(place, template);
-};
-
 const headerElement = document.querySelector(`.header`);
-render(headerElement, createUserScoreTemplate(), `beforeend`);
+render(headerElement, new UserScoreView().getElement(), RenderPosition.BEFOREEND);
 
 const mainElement = document.querySelector(`.main`);
-render(mainElement, createNavigationTemplate(), `beforeend`);
+renderTemplate(mainElement, createNavigationTemplate(), `beforeend`);
 
 const mainNavigation = mainElement.querySelector(`.main-navigation`);
-render(mainNavigation, createFilterTemplate(filters), `afterbegin`);
+renderTemplate(mainNavigation, createFilterTemplate(filters), `afterbegin`);
 
-render(mainElement, createSortingTemplate(), `beforeend`);
-render(mainElement, createFilmsPanelTemplate(), `beforeend`);
+renderTemplate(mainElement, createSortingTemplate(), `beforeend`);
+renderTemplate(mainElement, createFilmsPanelTemplate(), `beforeend`);
 
 const filmsPanelElement = mainElement.querySelector(`.films`);
-render(filmsPanelElement, createFilmsListTemplate(), `beforeend`);
+renderTemplate(filmsPanelElement, createFilmsListTemplate(), `beforeend`);
 
 const filmsListContainer = filmsPanelElement.querySelector(`.films-list__container`);
 
 for (let i = 0; i < Math.min(filmCards.length, CARDS_PER_STEP); i++) {
-  render(filmsListContainer, createFilmCardTemplate(filmCards[i]), `beforeend`);
+  renderTemplate(filmsListContainer, createFilmCardTemplate(filmCards[i]), `beforeend`);
 }
 
 if (filmCards.length > CARDS_PER_STEP) {
   let renderedCardsCount = CARDS_PER_STEP;
 
-  render(filmsListContainer, createShowMoreButtonTemplate(), `afterend`);
+  renderTemplate(filmsListContainer, createShowMoreButtonTemplate(), `afterend`);
 
   const showMoreButton = filmsPanelElement.querySelector(`.films-list__show-more`);
 
@@ -59,7 +56,7 @@ if (filmCards.length > CARDS_PER_STEP) {
 
     filmCards
       .slice(renderedCardsCount, renderedCardsCount + CARDS_PER_STEP)
-      .forEach((filmCard) => render(filmsListContainer, createFilmCardTemplate(filmCard), `beforeend`));
+      .forEach((filmCard) => renderTemplate(filmsListContainer, createFilmCardTemplate(filmCard), `beforeend`));
 
     renderedCardsCount += CARDS_PER_STEP;
 
@@ -70,26 +67,26 @@ if (filmCards.length > CARDS_PER_STEP) {
 }
 
 
-render(filmsPanelElement, createFilmsListExtraTemplate(), `beforeend`);
-render(filmsPanelElement, createFilmsListExtraTemplate(), `beforeend`);
+renderTemplate(filmsPanelElement, createFilmsListExtraTemplate(), `beforeend`);
+renderTemplate(filmsPanelElement, createFilmsListExtraTemplate(), `beforeend`);
 
 const extraFilmsElements = filmsPanelElement.querySelectorAll(`.films-list--extra`);
 
 const topRatedContainer = extraFilmsElements[0].querySelector(`.films-list__container`);
 for (let j = 0; j < EXTRA_CARDS_COUNT; j++) {
-  render(topRatedContainer, createFilmCardTemplate(extraFilmCardsTopRated[j]), `beforeend`);
+  renderTemplate(topRatedContainer, createFilmCardTemplate(extraFilmCardsTopRated[j]), `beforeend`);
 }
 
 const mostCommentedContainer = extraFilmsElements[1].querySelector(`.films-list__container`);
 for (let j = 0; j < EXTRA_CARDS_COUNT; j++) {
-  render(mostCommentedContainer, createFilmCardTemplate(extraFilmCardsMostCommented[j]), `beforeend`);
+  renderTemplate(mostCommentedContainer, createFilmCardTemplate(extraFilmCardsMostCommented[j]), `beforeend`);
 }
 
 const footerElement = document.querySelector(`.footer`);
 const footerStatisticsElement = footerElement.querySelector(`.footer__statistics`);
-render(footerStatisticsElement, createStatisticsTemplate(), `beforeend`);
+renderTemplate(footerStatisticsElement, createStatisticsTemplate(), `beforeend`);
 
-render(footerElement, createFilmDetailsPopupTemplate(filmCards[0]), `afterend`);
+renderTemplate(footerElement, createFilmDetailsPopupTemplate(filmCards[0]), `afterend`);
 
 const filmDetailsPopupElement = document.querySelector(`.film-details`);
 const popupCloseButton = filmDetailsPopupElement.querySelector(`.film-details__close-btn`);
