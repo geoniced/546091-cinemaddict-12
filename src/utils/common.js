33e19289a7da4cd0d136f312fb3e441933ce3246
Ivan Kashin <barbos1701@gmail.com>
2020-08-23 12:13:22 +0300

    Добавляет файлы utils/

---
+++ src/utils/common.js
@@ -0,0 +1,18 @@
+export const getUniqueArray = (array) => {
+  return [...new Set(array)];
+};
+
+export const getRandomInteger = (a = 0, b = 1) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+
+  return Math.floor(lower + Math.random() * (upper - lower + 1));
+};
+
+export const getRandomItem = (collection) => {
+  return collection[getRandomInteger(0, collection.length - 1)];
+};
+
+export const humanizeDate = (date) => {
+  return date.toLocaleString(`en-US`, {day: `numeric`, month: `long`, year: `numeric`});
+};
