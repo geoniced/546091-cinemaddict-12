b661a3547a271762a3f17c54f46e3178458b701f
Ivan Kashin <barbos1701@gmail.com>
2020-09-11 13:05:09 +0300

    Добавляет методы в модель комментариев, чтобы она соответствовала описанным действиям пользователя в const.js

--- src/model/comments.js
+++ src/model/comments.js
@@ -34,4 +34,28 @@ export default class Comments extends Observer {
 
     this._notify(updateType, update);
   }
+
+  addComment(updateType, update) {
+    this._comments = [
+      ...this._comments,
+      update
+    ];
+
+    this._notify(updateType, update);
+  }
+
+  deleteComment(updateType, update) {
+    const index = this._comments.findIndex((comment) => comment.id === update.id);
+
+    if (index === -1) {
+      throw new Error(`Can't update unexisting film`);
+    }
+
+    this._comments = [
+      ...this._comments.slice(0, index),
+      ...this._comments.slice(index + 1)
+    ];
+
+    this._notify(updateType);
+  }
 }
